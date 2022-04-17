be8e50a33b40b1d4994c38d628d5192c152d7432
sumaya99 <sumaya.shahoyi@gmail.com>
2022-04-16 18:31:58 -0700

    change

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'authentication.dart';
 import 'login.dart';
 import 'root_page.dart';
 
-
 void main() => runApp(MyApp());
-BaseAuth auth=new Auth();
+BaseAuth auth = new Auth();
 
 class MyApp extends StatelessWidget {
   @override
@@ -14,23 +13,23 @@ class MyApp extends StatelessWidget {
         title: 'Ledi App',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          cursorColor:Color(0xff8731DC),
+          cursorColor: Color(0xff8731DC),
           primaryColor: Color(0xff8731DC),
           accentColor: Color(0xffE7CFFF),
           bottomAppBarColor: Color(0xff120023),
-          dividerColor:Color(0xffE7CFFF),
-          buttonTheme:ButtonThemeData(
-              buttonColor: Color(0xff8731DC)),
+          dividerColor: Color(0xffE7CFFF),
+          buttonTheme: ButtonThemeData(buttonColor: Color(0xff8731DC)),
           accentColorBrightness: Brightness.dark,
-          textTheme:TextTheme(
+          textTheme: TextTheme(
 //            headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-             title: TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic,color:Color(0xff120023)),
-            body1: TextStyle( fontFamily: 'Montserrat'),
+            subtitle1: TextStyle(
+                fontSize: 18.0,
+                fontStyle: FontStyle.italic,
+                color: Color(0xff120023)),
+            bodyText1: TextStyle(fontFamily: 'Montserrat'),
           ),
-
-    ),
-      home: new RootPage(auth: new Auth())
-    );
+        ),
+        home: new RootPage(auth: new Auth()));
   }
 }
 
